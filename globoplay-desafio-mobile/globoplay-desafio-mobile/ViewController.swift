df0eac832e86d1ea858f12e3f03f6c411322080e
Carolina Ortega <cahortega@outlook.com>
2024-11-26 22:18:48 -0300

    Create xcode project

---
+++ globoplay-desafio-mobile/globoplay-desafio-mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  globoplay-desafio-mobile
+//
+//  Created by Carolina Ortega on 26/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
